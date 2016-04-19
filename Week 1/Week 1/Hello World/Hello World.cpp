790b88498f58b997be122539285e42812c18fd01
Mauro de Lyon <maurodelyon@gmail.com>
2016-04-19 10:46:45 +0200

    Helllo World

---
+++ Week 1/Week 1/Hello World/Hello World.cpp
@@ -0,0 +1,13 @@
+// Hello World.cpp : Defines the entry point for the console application.
+//
+// A hello world program in C++
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!\n";
+	return 0;
+}
